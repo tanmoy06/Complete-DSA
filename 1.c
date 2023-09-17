c0b97eb5927fe67c60ea2e941ee5b371acbd4ab8
Tanmoy Sarkar <iamtanmoysarkar007@gmail.com>
2023-09-18 00:54:56 +0530

    deleted sample code

--- 1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-    printf("hello");
-    printf("hii");
-    return 0;
-}
--- 2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-    int a = 5;
-    int b = 6;
-    printf(a+b);
-    return 0;
-}
